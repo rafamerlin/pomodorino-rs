627d0ded708c82c755bd02af8d0d75b40a4876db
Rafael Merlin <rafamerlin@gmail.com>
2021-09-03 00:46:14 +1000

    Fix timer length and add beep sound at the end

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,7 +5,9 @@
 
 mod icongen;
 
+use rodio::{self, Source};
 use std::{
+  io::{self},
   sync::{Arc, Mutex},
   thread,
 };
@@ -20,8 +22,11 @@ trait ToMessage: Send {
   fn value(&self) -> Vec<u8>;
 }
 
+const TIME_MULTIPLIER: f32 = 60.0;
+const BEEP: &[u8] = include_bytes!("../resources/ring.mp3");
+
 fn main() {
-  let pomodoro_time = Arc::new(Mutex::new(99));
+  let pomodoro_time = Arc::new(Mutex::new(99999.0));
 
   // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
   let quit = CustomMenuItem::new("quit".to_string(), "Quit");
@@ -40,19 +45,33 @@ fn main() {
   let (tx, rx) = crossbeam::channel::unbounded();
 
   let p_time = pomodoro_time.clone();
-  thread::spawn(move || loop {
-    let (tx_timer, rx_timer) = crossbeam::channel::unbounded();
-    let timer = timer::Timer::new();
-    let _guard = timer.schedule_with_delay(chrono::Duration::milliseconds(1000), move || {
-      let _ignored = tx_timer.send(());
-    });
-    rx_timer.recv().unwrap();
+  thread::spawn(move || {
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+    let sound = Sound(Arc::new(BEEP.to_vec()));
 
-    {
-      let mut p_time = p_time.lock().unwrap();
-      if *p_time > 0 && *p_time != 99 {
-        *p_time -= 1;
-        tx.send(*p_time).unwrap();
+    loop {
+      let (tx_timer, rx_timer) = crossbeam::channel::unbounded();
+      let timer = timer::Timer::new();
+      let _guard = timer.schedule_with_delay(chrono::Duration::milliseconds(1000), move || {
+        let _ignored = tx_timer.send(());
+      });
+      rx_timer.recv().unwrap();
+
+      {
+        let mut p_time = p_time.lock().unwrap();
+        if *p_time >= 0.0 && *p_time != 99999.0 {
+          if *p_time % TIME_MULTIPLIER <= 0.0 {
+            let minutes = (*p_time / TIME_MULTIPLIER).ceil() as usize;
+            tx.send(minutes).unwrap();
+          }
+          if *p_time == 0.0 {
+            stream_handle
+              .play_raw(sound.decoder().convert_samples())
+              .unwrap();
+          }
+
+          *p_time -= 1.0;
+        }
       }
     }
   });
@@ -63,15 +82,15 @@ fn main() {
       SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
         "p25" => {
           let mut p_time = pomodoro_time.lock().unwrap();
-          *p_time = 25;
+          *p_time = 25.0 * TIME_MULTIPLIER;
         }
         "p15" => {
           let mut p_time = pomodoro_time.lock().unwrap();
-          *p_time = 15;
+          *p_time = 15.0 * TIME_MULTIPLIER;
         }
         "p5" => {
           let mut p_time = pomodoro_time.lock().unwrap();
-          *p_time = 5;
+          *p_time = 5.0 * TIME_MULTIPLIER;
         }
         "quit" => {
           std::process::exit(0);
@@ -92,7 +111,7 @@ fn main() {
       let rx = rx.clone();
       tauri::async_runtime::spawn(async move {
         while let Ok(i) = rx.recv() {
-          if i == 99 {
+          if i == 99999 {
             tray_handle
               .set_icon(tauri::Icon::Raw(icongen::TOMATO_IMAGE.to_vec()))
               .unwrap();
@@ -109,4 +128,21 @@ fn main() {
     })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+pub struct Sound(Arc<Vec<u8>>);
+
+impl AsRef<[u8]> for Sound {
+  fn as_ref(&self) -> &[u8] {
+    &self.0
+  }
+}
+
+impl Sound {
+  pub fn cursor(&self) -> io::Cursor<Sound> {
+    io::Cursor::new(Sound(self.0.clone()))
+  }
+  pub fn decoder(&self) -> rodio::Decoder<io::Cursor<Sound>> {
+    rodio::Decoder::new(self.cursor()).unwrap()
+  }
 }
